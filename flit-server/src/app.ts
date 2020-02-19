2768b1067ffea004d53f80c3eb36ea9741e2e24c
Jason Wodicka <jason@wodicka.net>
2020-02-18 16:36:26 -0800

    Set server to port 4000 by default to avoid conflicts.

--- flit-server/src/app.ts
+++ flit-server/src/app.ts
@@ -38,7 +38,7 @@ mongoose.connect(mongoUrl, { useNewUrlParser: true, useCreateIndex: true, useUni
 });
 
 // Express configuration
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 4000);
 app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "pug");
 app.use(compression());
