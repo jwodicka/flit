9a4999e894e494b03a542d0ee54126f535c16667
Jason Wodicka <jason@wodicka.net>
2020-02-26 18:41:34 -0800

    replace the React logo with something a bit more Flit (#47)
    * replace the React logo with something a bit more Flit
    
    * update test to reflect change

--- flit-client/src/App.tsx
+++ flit-client/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './hummingbird.svg';
 import './App.css';
 
 function App() {
@@ -8,16 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Flit begins here.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
