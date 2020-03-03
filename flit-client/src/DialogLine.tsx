146513ac51f38bd8a74411e3082071ee0fecb5c8
Ben Yackley <august@fixl.co>
2020-03-02 18:13:44 -0800

    add a DialogLine component (#57)
    * add a DialogLine component
    
    Bare-bones for now, but should work fine for testing.
    
    * address comments and add more demo

---
+++ flit-client/src/DialogLine.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import face from './face.svg';
+import './DialogLine.css';
+
+type Props = {
+    // avatar: HTMLElement, // unused for now
+    name: string,
+    line: string,
+    timestamp: Date,
+}
+
+function makeTimestamp(stamp: Date):string {
+    const now = new Date();
+
+    const dayString = stamp.toDateString() === now.toDateString() ?
+        'Today' : stamp.toLocaleDateString();
+    const timeString = stamp.toLocaleTimeString();
+
+    return `${dayString} at ${timeString}`
+}
+
+export default function render(props: Props) {
+    return <div className='dialogLine'>
+        <div className='face'>
+            <img src={face} width={32} height={32} alt=''/>
+        </div>
+        <div className='name'>
+            {props.name}
+        </div>
+        <div className='timestamp'>
+            {makeTimestamp(props.timestamp)}
+        </div>
+        <div className='line'>
+            {props.line}
+        </div>
+    </div>
+}
