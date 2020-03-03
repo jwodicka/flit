18909f0bc827b880a314e5a75fb7c0aff276996a
Jett Jones <JettJones@users.noreply.github.com>
2020-03-03 15:25:25 -0800

    Graphql subscribe demo (#64)
    * Working subscription demo
    
    The send mutation will cause messages to be sent to current subscribers, although it does not persist them, and they do not appear if you request the message history.
    
    Co-authored-by: Jason Wodicka <jason@wodicka.net>

--- flit-server/src/controllers/graphql-resolver.ts
+++ flit-server/src/controllers/graphql-resolver.ts
@@ -4,12 +4,16 @@ import {
 } from "../types/generated/graphql";
 
 import {Chat, MessageSet} from "../models/Chat";
+import { PubSub } from "graphql-subscriptions";
 
 export class Context {
     models: {
         chatModel: Chat;
     };
 };
+
+const pubsub = new PubSub();
+const MESSAGE_ADDED = "MESSAGE_ADDED";
 
 function toGraphqlMessages(messageSet: MessageSet): MessageResult {
     return {
@@ -19,12 +23,24 @@ function toGraphqlMessages(messageSet: MessageSet): MessageResult {
     };
 }
 export const resolvers: Resolvers = {
+    Mutation: {
+        sendMessage(root, {body, senderId}, context) {
+            const added = context.models.chatModel.addMessage(body, senderId);
+            pubsub.publish(MESSAGE_ADDED, { messageAdded: added });
+            return added;
+          },
+    },
     Query: {
         getMessages: (root, args, ctx) => {
             const count = args.pageSize || 10;
             const after = args.after || null;
             const messages = ctx.models.chatModel.getMessages(count, after);
             return toGraphqlMessages(messages);
+        }
+    },
+    Subscription: {
+        messageAdded: {
+            subscribe: () => pubsub.asyncIterator([MESSAGE_ADDED])
         }
     }
 };
