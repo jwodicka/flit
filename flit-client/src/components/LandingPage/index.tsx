38f9599e859baf337a55601c9c61cb4321b5bcc5
BJ Yackley <cc@elseways.org>
2020-03-04 17:29:26 -0800

    move and rename files
    This is a small organizational change that gives each component its own directory inside components/ with index.tsx and styles.css as standard.

--- flit-client/src/components/LandingPage/index.tsx
+++ flit-client/src/components/LandingPage/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from './hummingbird.svg';
-import './LandingPage.css';
+import logo from '../hummingbird.svg';
+import './styles.css';
 
 function LandingPage() {
   return (
