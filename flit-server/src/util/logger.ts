300dae8ed304c30ef51a1d12ff3650cd96a2482e
Jason Wodicka <jason@wodicka.net>
2020-03-18 15:10:41 -0700

    improve logging based on NODE_ENV

--- flit-server/src/util/logger.ts
+++ flit-server/src/util/logger.ts
@@ -1,18 +1,27 @@
 import winston from "winston";
 
+const getTransports = (nodeEnv: string) => {
+    switch(nodeEnv) {
+        case "production":
+            return [new winston.transports.Console({level: "warn"})];
+        case "test":
+            return [];
+        case "dev":
+        case "development":
+        default:
+            return [
+                new winston.transports.Console({level: "warn"}),
+                new winston.transports.File({ filename: "debug.log", level: "debug" }),
+            ];
+    }
+}
+
 const options: winston.LoggerOptions = {
-    transports: [
-        new winston.transports.Console({
-            level: process.env.NODE_ENV === "production" ? "error" : "debug"
-        }),
-        new winston.transports.File({ filename: "debug.log", level: "debug" })
-    ]
+    transports: getTransports(process.env.NODE_ENV),
 };
 
 const logger = winston.createLogger(options);
 
-if (process.env.NODE_ENV !== "production") {
-    logger.debug("Logging initialized at debug level");
-}
+logger.debug("Logging initialized.");
 
 export default logger;
